4d13ddeefc937f8eddc74bd3f18cd19c23709fca
Sangeetha6300 <60879110+Sangeetha6300@users.noreply.github.com>
2020-02-11 16:35:12 +0530

    Add files via upload

---
+++ sanvi.java
@@ -0,0 +1,29 @@
+public class sanvi
+{
+public static void main(String args[])
+{
+byte b= 100;
+int s= 434;
+
+double sineval = 12345.234e;
+boolean flag = true;
+boolean val = false;
+ char ch1 = 55;
+char ch2 = 'Y';
+System.out.println("byte value =" +b);
+System.out.println("short value =" +s);
+System.out.println("int value =" +v);
+System.out.println("int second value =" +calc);
+System.out.println("long value =" +amountval);
+System.out.println("float value =" +intrestrate);
+System.out.println("double value =" +sineval);
+System.out.println("boolean value =" +flag);
+System.out.println("boolean value +" +val);
+System.out.println("char value =" +ch1);
+System.out.println("char value =" +ch2);
+}
+}
+
+
+
+
